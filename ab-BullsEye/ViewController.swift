e4e67555c3010cc704774913d288acec57ee2b30
Aaron Bradley <abeats20@gmail.com>
2015-03-15 18:31:42 -0500

    completed most of the basic functionalities of game play

--- ab-BullsEye/ViewController.swift
+++ ab-BullsEye/ViewController.swift
@@ -12,15 +12,20 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var slider: UISlider!
     @IBOutlet weak var targetLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var roundLabel: UILabel!
     
-    var currentValue: Int = 0
-    var targetValue: Int = 0
+    var currentValue = 0
+    var targetValue = 0
+    var score = 0
+    var round = 0
 
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        startNewRound()
+        startNewGame()
+        updateLabels()
     }
 
     override func didReceiveMemoryWarning() {
@@ -28,7 +33,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func startNewGame() {
+        score = 0
+        round = 0
+        startNewRound()
+    }
+
     func startNewRound() {
+        round++
         targetValue = 1 + Int(arc4random_uniform(100))
         currentValue = 50
         slider.value = Float(currentValue)
@@ -36,20 +48,52 @@ class ViewController: UIViewController {
         updateLabels()
     }
 
+    @IBAction func startOver() {
+        startNewGame()
+        updateLabels()
+    }
+
     func updateLabels() {
         targetLabel.text = String(targetValue)
+        scoreLabel.text = String(score)
+        roundLabel.text = String(round)
     }
 
     @IBAction func showAlert() {
-        let message = "The value of the slider is: \(currentValue)" + "\nThe target value is: \(targetValue)"
 
-        let alert = UIAlertController(title: "Hello Y'awll", message: message, preferredStyle: .Alert)
-        let action = UIAlertAction(title: "OK", style: .Default, handler: nil)
+        var difference = abs(currentValue - targetValue)
+        var points = 100 - difference
+
+        var title = String()
+
+        if difference == 0 {
+            title = "PERFECT HIT! DOUBLE POINTS!"
+            points += 100
+
+        } else if difference < 5 {
+            title = "Really close! Bonus Points!"
+            points += 50
+
+        } else if difference < 10 {
+            title = "Pretty good guess!"
+
+        } else {
+            title = "Dang...not even close..."
+        }
+
+        score += points
+
+        let message = "You scored \(points) points"
+
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .Alert)
+        let action = UIAlertAction(title: "OK", style: .Default, handler: { action in
+            self.startNewRound()
+            self.updateLabels()
+        })
 
         alert.addAction(action)
         presentViewController(alert, animated: true, completion: nil)
 
-        startNewRound()
     }
 
 
